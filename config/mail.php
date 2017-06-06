0b9649ae7ea89590582f1116b63148d6a9b174db
Liam Zhai <liamzhai@gmail.com>
2017-06-06 07:38:26 +0000

    modify reset mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'liamzhai@gmail.com', 'name' => 'Liam'],
 
     /*
     |--------------------------------------------------------------------------
