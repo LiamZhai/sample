e8e00e1d33664fea7b607cacb68f0674f8dcd053
Liam Zhai <liamzhai@gmail.com>
2017-06-09 06:41:16 +0000

    Add user statuses

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('UsersTableSeeder');
+         
+        $this->call('StatusesTableSeeder');
 
         Model::reguard();
     }
